a355572994c647226208dd467d93c967e1717cba
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-08 16:10:19 +0000

    used format string

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,6 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                  seperator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
-        message = re.sub(f'({field}=).*?{seperator}', r'\1{}{}'.
-                         format(redaction, seperator), message)
+        message = re.sub('({}=).*?{}'.format(fields, seperator),
+                         r'\1{}{}'.format(redaction, seperator), message)
     return message
