1ea1fc98f0dea854c8beb67a87caab134752b60e
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-08 16:17:03 +0000

    added more lines to docstring

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,8 +6,18 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  seperator: str) -> str:
-    """returns the log message obfuscated"""
+    """returns the log message obfuscated
+    Args:
+        fields (list of str): a list of strings representing all fields to
+            obfuscate
+        redaction (str): a string representing by what the field will be
+            obfucated
+        message (str): a string representing by which character is seperating
+            all fields in the log line message
+    Returns:
+        obfuscated str
+    """
     for field in fields:
-        message = re.sub('({}=).*?{}'.format(fields, seperator),
+        message = re.sub('({}=).*?{}'.format(field, seperator),
                          r'\1{}{}'.format(redaction, seperator), message)
     return message
