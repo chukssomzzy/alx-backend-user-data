29011338121c9734d82e674f04f7c3aeef64ac2b
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-08 15:41:15 +0000

    filetered_logger.py

---
+++ filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""filtered_logger"""
+import re
+
+
+def filter_datum(fields: str, redaction: str, message: str,
+                 seperator: str) -> str:
+    """returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(f'({field}=).*?{seperator}', r'\1{}'.
+                         format(redaction), message)
+    return message
