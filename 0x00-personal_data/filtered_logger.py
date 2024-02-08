b8ef8b37d9bdd6af36354ddfac44aab2f62092f4
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-08 16:06:05 +0000

    main.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,6 @@ def filter_datum(fields: str, redaction: str, message: str,
                  seperator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
-        message = re.sub(f'({field}=).*?{seperator}', r'\1{}'.
-                         format(redaction), message)
+        message = re.sub(f'({field}=).*?{seperator}', r'\1{}{}'.
+                         format(redaction, seperator), message)
     return message
--- filtered_logger.py
+++ filtered_logger.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
-"""filtered_logger"""
+"""
+implements a filtered_logger
+"""
 import re
 from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  seperator: str) -> str:
-    """returns the log message obfuscated"""
+    """returns the log message obfuscated
+    Args:
+        fields (list of str): a list of strings representing all fields to
+            obfuscate
+        message (str): a string representing the log line
+        seperator (str): a string representing by which character is seperating
+            all fields in the log line 'message'
+    Return:
+        obfuscated string
+    """
     for field in fields:
         message = re.sub(f'({field}=).*?{seperator}', r'\1{}'.
                          format(redaction), message)
