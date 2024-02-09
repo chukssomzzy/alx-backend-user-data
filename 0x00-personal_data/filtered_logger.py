02960bac55a5573d19f009cfc81658f798cee7dc
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-09 20:34:40 +0000

    password not a pil field

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ import re
 from typing import List, Tuple, Union
 import logging
 
-PII_FIELDS = ('email', 'phone', 'ssn', 'password', 'ip',)
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'ip',)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
