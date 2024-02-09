8abfc2539391de3255544f147172895b12ccaaa3
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-09 20:35:56 +0000

    what a pil field

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,10 +3,10 @@
 implemented filtered_logger
 """
 import re
-from typing import List, Tuple, Union
+from typing import List, Union
 import logging
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'ip',)
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'ip')
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
