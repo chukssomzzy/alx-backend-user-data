f8c12294c3d48cbfd4d51d688250514102b0b570
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-09 17:49:28 +0000

    format logs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,21 +3,22 @@
 implemented filtered_logger
 """
 import re
-from typing import List
+from typing import List, Tuple, Union
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 seperator: str) -> str:
+                 seperator: Union[str, None] = ';') -> str:
     """returns the log message obfuscated
     Args:
         fields (list of str): a list of strings representing all fields to
-            obfuscate
+        obfuscate
         redaction (str): a string representing by what the field will be
-            obfucated
+        obfucated
         message (str): a string representing by which character is seperating
-            all fields in the log line message
+        all fields in the log line message
         seperator (str): a string representing by which character is separating
-            all fields in the log line(message)
+        all fields in the log line(message)
     Returns:
         obfuscated str
     """
@@ -25,3 +26,30 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub('({}=).*?{}'.format(field, seperator),
                          r'\1{}{}'.format(redaction, seperator), message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize formatter
+        Args:
+            fields (list of string): a list of filter fields
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filters values in incoming log records using filter_datum
+        Args:
+            record (loggingrecord): A log record
+        """
+        message = super().format(record)
+        message = filter_datum(self.fields, self.REDACTION, message,
+                               self.SEPARATOR)
+        return message
