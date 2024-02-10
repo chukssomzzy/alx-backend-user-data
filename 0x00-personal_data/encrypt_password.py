328999db763aa0baf7d705a3615b57c3ca12254b
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-10 10:16:42 +0100

    utf-8

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     password
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if password matches the hash"""
+    return bcrypt.checkpw(hashed_password=hashed_password,
+                          password=password.encode("utf-8"))
