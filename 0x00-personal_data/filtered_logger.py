dc48389f0ccdab0eb714416cc05821742a7ccf4d
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-10 08:04:03 +0100

    ip is not pil

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ import re
 from typing import List, Union
 import logging
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'ip')
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
