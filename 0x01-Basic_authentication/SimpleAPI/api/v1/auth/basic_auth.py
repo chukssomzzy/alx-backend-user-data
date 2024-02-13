514662c0e5916e83dc8c817b401e9e421efa0fa5
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-13 16:05:15 +0000

    binary ascii

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """Basic Auth"""
 
 
+import base64
+import binascii
 from typing import Optional
 from api.v1.auth.auth import Auth
 
@@ -24,3 +26,22 @@ class BasicAuth(Auth):
                 or not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> Optional[str]:
+        """Decode the content of basic auth header
+        Args:
+            base64_authorization_header (str): base64 bytes of base64 header
+        Returns:
+            - returns the decoded str
+            - return None on error
+        """
+        if not base64_authorization_header or type(base64_authorization_header
+                                                   ) is not str:
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header)\
+                .decode('utf-8')
+        except binascii.Error:
+            return None
