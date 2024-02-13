c9fe3c9947a55e52be1c60a4b381a79be783642e
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-13 14:06:13 +0000

    IMPLEMENTED:
    [200~

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -14,8 +14,14 @@ class Auth:
         Returns:
             bool
         """
-        return False
-
+        if not path or not excluded_path:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_path:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """authorization header
