64a515a3ce74819809c6a2ecc684db3a99132ad7
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-14 14:24:25 +0000

    IMPLEMENTED: get a user_id from a session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -28,3 +28,11 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self,
+                               session_id: Optional[str] = None
+                               ) -> Optional[str]:
+        """Get a user_id associated with a session id"""
+        if not session_id or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
