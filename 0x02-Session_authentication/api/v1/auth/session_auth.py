976ac920a12b1755f7150faa226bcc914780d5df
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-14 14:13:36 +0000

    UPDATE: session_auth with ability to create a session for each authenticated user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,8 @@ Session authentication for simple API
 """
 
 
+from typing import Optional
+import uuid
 from api.v1.auth.auth import Auth
 
 
@@ -11,3 +13,18 @@ class SessionAuth(Auth):
     """Implements session authentication for
     flask application
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: Optional[str] = None) -> Optional[str]:
+        """Create a inmemory login session for each authenticated user
+        Args:
+            user_id (str): the authenticated user id
+        Returns:
+            - (str) The session id
+            - (None) on error
+        """
+        if not user_id or type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
