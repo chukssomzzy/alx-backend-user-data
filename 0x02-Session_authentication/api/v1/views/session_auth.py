08ab03ac94579c33710afd681b9fd5cff14b7772
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-14 17:37:03 +0000

    IMPLEMENTED: login route for authentication

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Implements auth routes
+"""
+from os import getenv
+from flask import jsonify, make_response, request
+from werkzeug.exceptions import abort
+from api.v1.views import app_views
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=["POST"], strict_slashes=False)
+def login():
+    """Login authentication route
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    elif not password:
+        return jsonify({"error": "password missing"}), 400
+    user = User.search({"email": email})
+    if not user:
+        return jsonify({'error': "no user found for this email"}), 404
+    user = user[0]
+    if not user.is_valid_password(password):
+        return jsonify({'error': "wrong password"}), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user_id=str(user.id))
+    resp = make_response(user.to_json())
+    session_name = getenv('SESSION_NAME')
+    if not session_name:
+        abort(500)
+    resp.set_cookie(session_name, session_id)
+    return resp
