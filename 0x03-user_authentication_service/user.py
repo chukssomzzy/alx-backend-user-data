98ea7f66e2a4167ee452c77dafa04aa2c4db99cc
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-21 01:31:01 +0100

    IMPLEMENTED: added user model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Mapping for user"""
+from sqlalchemy import VARCHAR, Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """Defines A user Mapping"""
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(VARCHAR(250), nullable=False)
+    hashed_password = Column(VARCHAR(250), nullable=False)
+    session_id = Column(VARCHAR(250))
+    reset_token = Column(VARCHAR(250))
