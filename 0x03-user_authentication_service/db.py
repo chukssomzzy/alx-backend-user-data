165a748f94b907c05785123642e07ea73e55440e
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-21 10:57:24 +0000

    UPDATED: remove required arguments from find_user_by

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -44,7 +44,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, *args, **kwargs):
+    def find_user_by(self, **kwargs):
         """Filter user table by kwargs
         args:
             args (array of str): not used
