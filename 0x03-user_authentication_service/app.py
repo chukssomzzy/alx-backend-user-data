161dea258ea930c52d3eed9f7a9c09a8266ac1e3
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-21 13:44:41 +0000

    IMPLEMENTED: create user endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """Implements flask endpoints"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+
+from auth import Auth
 
 app = Flask(__name__)
+auth = Auth()
 
 
 @app.route("/", strict_slashes=False)
@@ -10,6 +13,30 @@ def index():
     """A basic flask endpoint
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=["POST"], strict_slashes=False)
+def users():
+    """Register user endpoint
+    Forms:
+        email (str): user email
+        password (str): user password
+    Response:
+        200:
+            registeredUser:
+                email: str
+                message: str
+        400:
+            alreadyRegisteredUser:
+                message: str
+    """
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+        auth.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
