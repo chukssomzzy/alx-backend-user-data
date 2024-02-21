f35bd638667e0be5c39024208a8ecfa8e345aa76
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-21 01:47:17 +0100

    IMPLEMENTED: add user to db

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """add a user object to db
+        Args:
+            email (str): email for user
+            hashed_password (str): hashed_password for user
+        Returns:
+            - User
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Mapping for user"""
-from sqlalchemy import VARCHAR, Column, Integer
+from sqlalchemy import VARCHAR, Column, Integer, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -9,7 +9,7 @@ Base = declarative_base()
 class User(Base):
     """Defines A user Mapping"""
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     email = Column(VARCHAR(250), nullable=False)
     hashed_password = Column(VARCHAR(250), nullable=False)
     session_id = Column(VARCHAR(250))
