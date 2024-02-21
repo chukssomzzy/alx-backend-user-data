3632744ed94baefd6aa419427fbc2ff0e5f1563e
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-21 15:08:49 +0000

    IMPLEMENTED: sessions route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Implements flask endpoints"""
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, make_response, request
 
 from auth import Auth
 
@@ -37,6 +37,32 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=["POST"], strict_slashes=False)
+def login():
+    """Login a user to the user management service
+    Forms:
+        email (str): the user email
+        password (str): the user password
+    Response:
+        200:
+            userLogged:
+                email: str
+                message: str
+        401:
+            None
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password or not auth.valid_login(email, password):
+        abort(401)
+    session_id = auth.create_session(email)
+    if not session_id:
+        abort(401)
+    resp = make_response(jsonify({"email": email, "message": "logged in"}))
+    resp.set_cookie("session_id", session_id)
+    return resp
 
 
 if __name__ == "__main__":
